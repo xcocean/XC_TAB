28f65d73022cd0478761ad40356aedeaf232ecaf
lingkang <910405656@qq.com>
2019-03-24 15:52:16 +0800

    Initialization

---
+++ app/src/main/java/xcocean/com/xc_tab/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package xcocean.com.xc_tab.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xcocean.com.xc_tab.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
